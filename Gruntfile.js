790e3ef3084fd5ffe3c8b3d3b0837197c207daa8
Hariadi Hinta <diperakui@yahoo.com>
2014-04-12 11:14:12 +0800

    update deps and remove grunt-readme
    use verb instead

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,13 +64,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-readme');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'wp2md', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test', 'readme']);
+  grunt.registerTask('default', ['jshint', 'test']);
 
 };
